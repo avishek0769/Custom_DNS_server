b47f963fd0a98977d554d8f0a08cbaf51b54c6dd
avishek0769 <avishekadhikary42@gmail.com>
2026-02-23 18:16:25 +0530

    Make it support DNS Resolutions for A and CNAME records using dig CLI

--- index.ts
+++ index.ts
@@ -1,18 +1,79 @@
 import dgram from 'node:dgram';
+import dnsPacket, { type Answer } from 'dns-packet';
 
 const server = dgram.createSocket('udp4');
 
-server.on('error', (err) => {
-    console.error(`Server error:\n${err.stack}`);
-    server.close();
-});
+const db: Record<string, { type: "A" | "CNAME"; data: string }> = {
+    "videotubes.app": {
+        type: "A",
+        data: "20.193.133.136"
+    },
+    "www.videotubes.app": {
+        type: "CNAME",
+        data: "videotubes.app"
+    },
+}
 
 server.on('message', (msg, rinfo) => {
-    console.log(`Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}`);
+    try {
+        const query: dnsPacket.DecodedPacket = dnsPacket.decode(msg)
+        const questionName = query.questions?.[0].name;
+        const dbResponse = questionName ? db[questionName as keyof typeof db] : undefined;
+        let response;
+    
+        if(dbResponse && questionName) {
+            let answers: Answer[] = []
+
+            if(dbResponse.type == "CNAME") {
+                answers.push({
+                    type: dbResponse.type as "A" | "CNAME",
+                    class: "IN",
+                    name: questionName,
+                    data: dbResponse.data
+                })
+                
+                const dbResponseForCname = db[dbResponse.data];
+                
+                answers.push({
+                    type: dbResponseForCname.type as "A" | "CNAME",
+                    class: "IN",
+                    name: dbResponse.data,
+                    data: dbResponseForCname.data
+                })
+            }
+            else if(dbResponse.type == "A") {
+                answers.push({
+                    type: dbResponse.type as "A" | "CNAME",
+                    class: "IN",
+                    name: questionName,
+                    data: dbResponse.data
+                })
+            }
+
+            response = dnsPacket.encode({
+                type: "response",
+                id: query.id,
+                flags: dnsPacket.AUTHORITATIVE_ANSWER,
+                questions: query.questions,
+                answers
+            })
+        }
+        else throw new Error("dbResponse or questionName not available")
+    
+        server.send(response, rinfo.port, rinfo.address)
+    }
+    catch (error) {
+        console.log("Error --> ", error)
+    }
 });
 
 server.on("listening", () => {
     console.log("UDP - DNS Server running on 2053...")
 })
+
+server.on('error', (err) => {
+    console.error(`Server error:\n${err.stack}`);
+    server.close();
+});
 
 server.bind(2053);
