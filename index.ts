c230030b47cba03751661607959cce6ae88652c2
avishek0769 <avishekadhikary42@gmail.com>
2026-02-23 21:31:25 +0530

    Organise by get_record functions and introduce zones with records instead of plain db object. Also return SOA, NS records

--- index.ts
+++ index.ts
@@ -3,73 +3,167 @@ import dnsPacket, { type Answer } from 'dns-packet';
 
 const server = dgram.createSocket('udp4');
 
-const db: Record<string, { type: "A" | "CNAME"; data: string }> = {
-    "videotubes.app": {
-        type: "A",
-        data: "20.193.133.136"
+type RecordType = {
+    type: "A" | "CNAME" | "SOA" | "NS";
+    data: string;
+};
+
+type Zone = {
+    origin: string;
+    admin: string;
+    serial: number;
+    records: Record<string, RecordType>;
+};
+
+const zones: Zone[] = [
+    {
+        origin: "videotubes.app",
+        admin: "admin.videotubes.app",
+        serial: 1,
+        records: {
+            "videotubes.app": {
+                type: "A",
+                data: "20.193.133.136"
+            },
+            "www.videotubes.app": {
+                type: "CNAME",
+                data: "videotubes.app"
+            }
+        }
     },
-    "www.videotubes.app": {
-        type: "CNAME",
-        data: "videotubes.app"
+    {
+        origin: "videocall.studio",
+        admin: "admin.videocall.studio",
+        serial: 1,
+        records: {
+            "videocall.studio": {
+                type: "A",
+                data: "20.193.133.136"
+            },
+            "www.videocall.studio": {
+                type: "CNAME",
+                data: "videocall.studio"
+            }
+        }
     },
-    "videocall.studio": {
-        type: "A",
-        data: "20.193.133.136"
+    {
+        origin: "chatgpt.com",
+        admin: "admin.chatgpt.com",
+        serial: 1,
+        records: {
+            "chatgpt.com": {
+                type: "A",
+                data: "172.64.155.209"
+            }
+        }
     },
-    "www.videocall.studio": {
-        type: "CNAME",
-        data: "videocall.studio"
-    },
-    "chatgpt.com": {
-        type: "A",
-        data: "172.64.155.209"
-    },
-    "google.com": {
-        type: "A",
-        data: "142.250.206.78"
-    },
-}
+    {
+        origin: "google.com",
+        admin: "admin.google.com",
+        serial: 1,
+        records: {
+            "google.com": {
+                type: "A",
+                data: "142.250.206.78"
+            }
+        }
+    }
+];
+
 // Command to forward outgoing UDP reqs to destination port 53 to local 2053 -
 // sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 2053
 
 // Command to stop port forwarding -
 // sudo iptables -t nat -D OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 2053
 
+const getCname_Record = (record: RecordType, questionName: string): Answer => {
+    return {
+        type: record.type as "A" | "CNAME",
+        class: "IN",
+        name: questionName,
+        data: record.data
+    }
+}
+
+const getA_Record = (record: RecordType, questionName: string): Answer => {
+    return {
+        type: record.type as "A" | "CNAME",
+        class: "IN",
+        name: questionName,
+        data: record.data,
+    }
+}
+
+function getSOA_Record(zone: Zone): Answer {
+    return {
+        type: "SOA",
+        name: zone.origin,
+        class: "IN",
+        data: {
+            mname: "ns.avishekadhikary.tech",
+            rname: zone.admin,
+            serial: zone.serial,
+            refresh: 300,
+            retry: 300,
+            expire: 1200,
+            minimum: 300
+        }
+    };
+}
+
+function getNS_Record(zone: Zone): Answer {
+    return {
+        type: "NS",
+        name: zone.origin,
+        class: "IN",
+        data: "ns.avishekadhikary.tech"
+    };
+}
+
 server.on('message', (msg, rinfo) => {
     try {
         const query: dnsPacket.DecodedPacket = dnsPacket.decode(msg)
-        const questionName = query.questions?.[0].name;
-        const dbResponse = questionName ? db[questionName as keyof typeof db] : undefined;
+        const question = query.questions?.[0];
+        const zone: Zone | undefined = question && zones.find(z => question.name.endsWith(z.origin))
         let response;
-    
-        if(dbResponse && questionName) {
-            console.log("Query --> ", query.questions?.[0].name)
-            let answers: Answer[] = []
 
-            if(dbResponse.type == "CNAME") {
-                answers.push({
-                    type: dbResponse.type as "A" | "CNAME",
-                    class: "IN",
-                    name: questionName,
-                    data: dbResponse.data
-                })
-                
-                const dbResponseForCname = db[dbResponse.data];
-                
-                answers.push({
-                    type: dbResponseForCname.type as "A" | "CNAME",
-                    class: "IN",
-                    name: dbResponse.data,
-                    data: dbResponseForCname.data
-                })
+        if (zone && question) {
+            const record = zone.records[question.name]
+            console.log("Question --> ", query.questions)
+            
+            let answers: Answer[] = []
+            let authorities: Answer[] = []
+
+            if (record && question.type == "CNAME" && record.type == question.type) {
+                answers.push(getCname_Record(record, question.name))
+
+                const recordForA = zone.records[record.data]
+                answers.push(getA_Record(recordForA, record.data))
             }
-            else if(dbResponse.type == "A") {
-                answers.push({
-                    type: dbResponse.type as "A" | "CNAME",
-                    class: "IN",
-                    name: questionName,
-                    data: dbResponse.data
-                })
+            else if (record && question.type == "A" && record.type == question.type) {
+                answers.push(getA_Record(record, question.name))
+            }
+            else if(record && question.type == "SOA" && record.type == question.type) {
+                answers.push(getSOA_Record(zone))
+            }
+            else if(record && question.type == "NS" && record.type == question.type) {
+                answers.push(getNS_Record(zone))
+            }
+            // If domain exists but record type not supported
+            else if (question.name.endsWith(zone.origin)) {
+                authorities.push(getSOA_Record(zone));
+            }
+            else {
+                response = dnsPacket.encode({
+                    type: "response",
+                    id: query.id,
+                    flags: dnsPacket.AUTHORITATIVE_ANSWER,
+                    questions: query.questions,
+                    authorities: [getSOA_Record(zone)]
+                });
+
+                server.send(response, rinfo.port, rinfo.address);
+                return;
             }
 
             response = dnsPacket.encode({
@@ -77,11 +171,12 @@ server.on('message', (msg, rinfo) => {
                 id: query.id,
                 flags: dnsPacket.AUTHORITATIVE_ANSWER,
                 questions: query.questions,
-                answers
+                authorities,
+                answers,
             })
 
             server.send(response, rinfo.port, rinfo.address)
-            console.log("Answer --> ", query.questions?.[0].name)
+            console.log("Answer --> ", answers)
         }
     }
     catch (error) {
