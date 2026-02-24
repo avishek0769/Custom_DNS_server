ad1a5918a3d7c487c99889eb95c520a26924c4c9
avishek0769 <avishekadhikary42@gmail.com>
2026-02-24 20:53:14 +0530

    Fix ts errors and build the js code

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-import * as dgram from 'node:dgram';
-import * as dnsPacket from 'dns-packet';
-import type { Answer } from 'dns-packet';
-
-const server = dgram.createSocket('udp4');
-
-type RecordType = {
-    type: "A" | "CNAME" | "SOA" | "NS";
-    data: string;
-};
-
-type Zone = {
-    origin: string;
-    admin: string;
-    serial: number;
-    records: Record<string, RecordType>;
-};
-
-const zones: Zone[] = [
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dgram = require("node:dgram");
+var dnsPacket = require("dns-packet");
+var server = dgram.createSocket('udp4');
+var zones = [
     {
         origin: "videotubes.app",
         admin: "admin.videotubes.app",
@@ -70,32 +57,27 @@ const zones: Zone[] = [
         }
     }
 ];
-
 // Command to forward outgoing UDP reqs to destination port 53 to local 2053 -
 // sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 2053
-
 // Command to stop port forwarding -
 // sudo iptables -t nat -D OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 2053
-
-const getCname_Record = (record: RecordType, questionName: string): Answer => {
+var getCname_Record = function (record, questionName) {
     return {
-        type: record.type as "A" | "CNAME",
+        type: record.type,
         class: "IN",
         name: questionName,
         data: record.data
-    }
-}
-
-const getA_Record = (record: RecordType, questionName: string): Answer => {
+    };
+};
+var getA_Record = function (record, questionName) {
     return {
-        type: record.type as "A" | "CNAME",
+        type: record.type,
         class: "IN",
         name: questionName,
         data: record.data,
-    }
-}
-
-function getSOA_Record(zone: Zone): Answer {
+    };
+};
+function getSOA_Record(zone) {
     return {
         type: "SOA",
         name: zone.origin,
@@ -111,8 +93,7 @@ function getSOA_Record(zone: Zone): Answer {
         }
     };
 }
-
-function getNS_Record(zone: Zone): Answer {
+function getNS_Record(zone) {
     return {
         type: "NS",
         name: zone.origin,
@@ -120,38 +101,34 @@ function getNS_Record(zone: Zone): Answer {
         data: "ns.avishekadhikary.tech"
     };
 }
-
-server.on('message', (msg, rinfo) => {
+server.on('message', function (msg, rinfo) {
+    var _a;
     try {
-        const query: dnsPacket.DecodedPacket = dnsPacket.decode(msg)
-        const question = query.questions?.[0];
-        const zone: Zone | undefined = question && zones.find(z => question.name.endsWith(z.origin))
-        let response;
-
-        if (zone && question) {
-            const record = zone.records[question.name]
-            console.log("Question --> ", query.questions)
-            
-            let answers: Answer[] = []
-            let authorities: Answer[] = []
-
-            if (record && question.type == "CNAME" && record.type == question.type) {
-                answers.push(getCname_Record(record, question.name))
-
-                const recordForA = zone.records[record.data]
-                answers.push(getA_Record(recordForA, record.data))
+        var query = dnsPacket.decode(msg);
+        var question_1 = (_a = query.questions) === null || _a === void 0 ? void 0 : _a[0];
+        var zone = question_1 && zones.find(function (z) { return question_1.name.endsWith(z.origin); });
+        var response = void 0;
+        if (zone && question_1) {
+            var record = zone.records[question_1.name];
+            console.log("Question --> ", query.questions);
+            var answers = [];
+            var authorities = [];
+            if (record && question_1.type == "CNAME" && record.type == question_1.type) {
+                answers.push(getCname_Record(record, question_1.name));
+                var recordForA = zone.records[record.data];
+                answers.push(getA_Record(recordForA, record.data));
             }
-            else if (record && question.type == "A" && record.type == question.type) {
-                answers.push(getA_Record(record, question.name))
+            else if (record && question_1.type == "A" && record.type == question_1.type) {
+                answers.push(getA_Record(record, question_1.name));
             }
-            else if(record && question.type == "SOA" && record.type == question.type) {
-                answers.push(getSOA_Record(zone))
+            else if (record && question_1.type == "SOA" && record.type == question_1.type) {
+                answers.push(getSOA_Record(zone));
             }
-            else if(record && question.type == "NS" && record.type == question.type) {
-                answers.push(getNS_Record(zone))
+            else if (record && question_1.type == "NS" && record.type == question_1.type) {
+                answers.push(getNS_Record(zone));
             }
             // If domain exists but record type not supported
-            else if (question.name.endsWith(zone.origin)) {
+            else if (question_1.name.endsWith(zone.origin)) {
                 authorities.push(getSOA_Record(zone));
             }
             else {
@@ -162,36 +139,30 @@ server.on('message', (msg, rinfo) => {
                     questions: query.questions,
                     authorities: [getSOA_Record(zone)]
                 });
-
                 server.send(response, rinfo.port, rinfo.address);
                 return;
             }
-
             response = dnsPacket.encode({
                 type: "response",
                 id: query.id,
                 flags: dnsPacket.AUTHORITATIVE_ANSWER,
                 questions: query.questions,
-                authorities,
-                answers,
-            })
-
-            server.send(response, rinfo.port, rinfo.address)
-            console.log("Answer --> ", answers)
+                authorities: authorities,
+                answers: answers,
+            });
+            server.send(response, rinfo.port, rinfo.address);
+            console.log("Answer --> ", answers);
         }
     }
     catch (error) {
-        console.log("Error --> ", error)
+        console.log("Error --> ", error);
     }
 });
-
-server.on("listening", () => {
-    console.log("UDP - DNS Server running on 2053...")
-})
-
-server.on('error', (err) => {
-    console.error(`Server error:\n${err.stack}`);
+server.on("listening", function () {
+    console.log("UDP - DNS Server running on 2053...");
+});
+server.on('error', function (err) {
+    console.error("Server error:\n".concat(err.stack));
     server.close();
 });
-
-server.bind(53, "0.0.0.0");
+server.bind(2053, "0.0.0.0");
